b512c5af6525db77c42828af85c69746ef9869f6
Oyama Jun (大山 順) <jun.oyama@cct-inc.co.jp>
2019-05-24 16:28:52 +0900

    せれくとボックス修正

--- src/features/userOrder/components/modal/components/OrderView.tsx
+++ src/features/userOrder/components/modal/components/OrderView.tsx
@@ -5,23 +5,32 @@ import { FilteredFavoritesList } from '../selectors';
 
 export const OrderView = () => {
   const { selectFavorites, selectOrderNumber } = useActions(Actions);
-  const { totalAmount, orderNumberList } = useMappedState(
-    state => state.userOrderModal
-  );
+  const {
+    totalAmount,
+    orderNumberList,
+    favorites,
+    orderNumber,
+  } = useMappedState(state => state.userOrderModal);
   return (
     <div style={{ width: '300px' }}>
       <p>すきなもの</p>
-      <select onChange={e => selectFavorites(e.target.value as Favorites)}>
+      <select
+        onChange={e => selectFavorites(e.target.value as Favorites)}
+        defaultValue={favorites}
+      >
         {FilteredFavoritesList().map((v, i) => {
           return (
-            <option key={i} value={v.id} selected={v.selected}>
+            <option key={i} value={v.id}>
               {v.name}
             </option>
           );
         })}
       </select>
       <p>数量</p>
-      <select onChange={e => selectOrderNumber(Number(e.target.value))}>
+      <select
+        onChange={e => selectOrderNumber(Number(e.target.value))}
+        defaultValue={String(orderNumber)}
+      >
         {orderNumberList.map((v: OrderNumber, i) => {
           return (
             <option key={i} value={v.id} selected={v.selected}>
