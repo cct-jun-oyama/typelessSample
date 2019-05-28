43e472252a85c107ca765d638173dc9325707501
Oyama Jun (大山 順) <jun.oyama@cct-inc.co.jp>
2019-05-28 10:36:41 +0900

    いったん削除

--- src/features/sampleApi/components/SampleApiView.tsx
+++ src/features/sampleApi/components/SampleApiView.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Dashboard } from 'src/components/Dashboard';
 import CatView from './cat/module';
-import StationSelectView from './stationSelect/module';
 
 export const SampleApiView = () => {
   return (
     <Dashboard>
       <h2>Sample API</h2>
       <CatView />
-      <StationSelectView />
     </Dashboard>
   );
 };
